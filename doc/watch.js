88ea00c6b71165c66b4272d3b93a9bf7f10e9938
Damian Senn <damian.senn@topaxi.codes>
2019-03-31 14:08:37 +0200

    Things

--- doc/watch.js
+++ doc/watch.js
@@ -1,22 +1,22 @@
-const { watch } = require('chokidar');
-const { execSync } = require('child_process');
-const Rx = require('rxjs');
-const rx = require('rxjs/operators');
+const { watch } = require('chokidar')
+const { execSync } = require('child_process')
+const Rx = require('rxjs')
+const rx = require('rxjs/operators')
 
 Rx.fromEvent(watch('.', { ignored: /[\/\\]\./ }), 'all', (event, path) => path)
   .pipe(rx.filter(shouldUpdate))
   .pipe(rx.debounceTime(1000))
-  .subscribe(generatePDF);
+  .subscribe(generatePDF)
 
 function generatePDF(path) {
   try {
-    console.log('%o rebuilding pdf...', new Date());
-    execSync('pdflatex main.tex -o main.pdf');
+    console.log('%o rebuilding pdf...', new Date())
+    execSync('pdflatex main.tex -o main.pdf')
   } catch (error) {
-    console.error(error);
+    console.error(error)
   }
 }
 
 function shouldUpdate(path) {
-  return /\.(?:tex|pdf|svg)/.test(path) && !/main.pdf$/.test(path);
+  return /\.(?:tex|pdf|svg)/.test(path) && !/main.pdf$/.test(path)
 }
