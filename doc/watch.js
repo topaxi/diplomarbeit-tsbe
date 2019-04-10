9e2adc1c2222fc32ffbfd7a8daa3dd02e5d745cc
Damian Senn <senn@puzzle.ch>
2019-04-10 12:20:32 +0200

    Cleanup aux files before rebuild

--- doc/watch.js
+++ doc/watch.js
@@ -1,7 +1,9 @@
+const fs = require('fs')
 const { watch } = require('chokidar')
 const { execSync } = require('child_process')
 const Rx = require('rxjs')
 const rx = require('rxjs/operators')
+const glob = require('glob')
 
 Rx.fromEvent(watch('.', { ignored: /[\/\\]\./ }), 'all', (event, path) => path)
   .pipe(rx.filter(shouldUpdate))
@@ -10,13 +12,24 @@ Rx.fromEvent(watch('.', { ignored: /[\/\\]\./ }), 'all', (event, path) => path)
 
 function generatePDF(path) {
   try {
-    console.log('%o rebuilding pdf...', new Date())
+    let time = new Date()
+    console.log('%o rebuilding pdf...', time)
     execSync('pdflatex main.tex -o main.pdf')
+    let done = new Date()
+    console.log('%o rebuilt pdf (%sms)', done, done - time)
   } catch (error) {
-    console.error(error)
+    console.error(String(error.stdout))
+    console.error(String(error.stderr))
   }
 }
 
 function shouldUpdate(path) {
   return /\.(?:tex|pdf|svg)/.test(path) && !/main.pdf$/.test(path)
+}
+
+// For some reasons, pdflatex sometimes gets stuck if these are lying around
+function cleanupAuxFiles() {
+  for (let file of glob.sync('**/*.aux')) {
+    fs.unlinkSync(file)
+  }
 }
