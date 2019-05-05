5723c0cbf8d09ee86492cc6bf11c9792826122d2
Damian Senn <damian.senn@gmail.com>
2019-05-05 22:08:20 +0200

    Set page header class

--- apps/gigpillar_web/lib/gigpillar_web/controllers/page_controller.ex
+++ apps/gigpillar_web/lib/gigpillar_web/controllers/page_controller.ex
@@ -2,6 +2,6 @@ defmodule GigpillarWeb.PageController do
   use GigpillarWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    render(conn, "index.html", page_class: 'index')
   end
 end
