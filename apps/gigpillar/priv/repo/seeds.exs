0eade7c0b4ffd1f15f0a4d30649f8ff40fce569c
Damian Senn <damian.senn@gmail.com>
2019-05-06 18:28:09 +0200

    Add genres from konzept to seed data

--- apps/gigpillar/priv/repo/seeds.exs
+++ apps/gigpillar/priv/repo/seeds.exs
@@ -9,3 +9,19 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+[
+  "Alternative",
+  "Blues",
+  "Classical",
+  "EDM",
+  "Hip-Hop",
+  "Jazz",
+  "Metal",
+  "Pop",
+  "Punk",
+  "Reggae",
+  "Rock"
+]
+|> Enum.map(fn genre -> %Gigpillar.Genres.Genre{name: genre} end)
+|> Enum.each(fn genre -> Gigpillar.Repo.insert!(genre) end)
