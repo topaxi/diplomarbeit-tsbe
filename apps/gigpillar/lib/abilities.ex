54b5c1a67980ebab0b4fe1b5ab6ef9c4ef68acc5
Damian Senn <damian.senn@topaxi.codes>
2019-05-14 16:42:11 +0200

    Comments

--- apps/gigpillar/lib/abilities.ex
+++ apps/gigpillar/lib/abilities.ex
@@ -2,17 +2,27 @@ alias Gigpillar.Accounts.User
 alias Gigpillar.Gigs.Gig
 
 defimpl Canada.Can, for: User do
-  def can?(%User{}, action, Gig) when action in [:index, :new, :create], do: true
+  # User can list their gigs and create new gigs
+  def can?(%User{}, action, Gig)
+      when action in [:index, :new, :create],
+      do: true
 
-  def can?(%User{id: user_id}, action, %Gig{creator_id: user_id})
+  # User can edit and delete their own gigs
+  def can?(%User{id: uid}, action, %Gig{creator_id: uid})
       when action in [:edit, :update, :delete],
       do: true
 
+  # User can show any gig
   def can?(%User{}, :show, %Gig{}), do: true
+
+  # User is not allowed to do anything else
   def can?(%User{}, _, _), do: false
 end
 
 defimpl Canada.Can, for: Atom do
+  # Anyone can show a specific gig
   def can?(nil, :show, %Gig{}), do: true
+
+  # By default, nothing is permitted
   def can?(nil, _, _), do: false
 end
