4ecd8828818e4f6234ede2af95cfcd22cdc678ba
Damian Senn <damian.senn@topaxi.codes>
2019-05-15 23:01:49 +0200

    Docs

--- apps/gigpillar_web/config/config.exs
+++ apps/gigpillar_web/config/config.exs
@@ -8,12 +8,14 @@ use Mix.Config
 config :gigpillar_web,
   ecto_repos: [Gigpillar.Repo],
   generators: [context_app: :gigpillar],
-  default_city: "Zurich"
+  default_city: System.get_env("GIGPILLAR_WEB_DEFAULT_CITY") || "Zurich"
 
 # Configures the endpoint
 config :gigpillar_web, GigpillarWeb.Endpoint,
-  url: [host: "localhost"],
-  secret_key_base: "OJnY+aiLLv/irP92vuF6KxLuMiIp2oySfogRBEmP/QYmr5b0mPW6wqRhkX/BSSXO",
+  url: [host: System.get_env("GIGPILLAR_WEB_HOST") || "localhost"],
+  secret_key_base:
+    System.get_env("GIGPILLAR_WEB_SECRET_KEY_BASE") ||
+      "wcR6neHuJjwErwFAbZPLUPGj5yo3ciue4dLRNz6ECwQImSGLYhL0T5PexUfyx/zC",
   render_errors: [view: GigpillarWeb.ErrorView, accepts: ~w(html json)],
   pubsub: [name: GigpillarWeb.PubSub, adapter: Phoenix.PubSub.PG2]
 
@@ -31,19 +33,22 @@ config :ueberauth, Ueberauth,
 config :plug_session_redis, :config,
   name: :gigpillar_sessions,
   pool: [size: 2, max_overflow: 5],
-  redis: [host: '127.0.0.1', port: 6379]
+  redis: [
+    host: System.get_env("REDIS_HOST") || "127.0.0.1",
+    port: System.get_env("REDIS_PORT") || 6379
+  ]
 
 config :canary, repo: Gigpillar.Repo
 
 config :google_api_client,
-  api_key: ""
+  api_key: System.get_env("GOOGLE_API_KEY")
 
 config :geolix,
   databases: [
     %{
       id: :mmdb2,
       adapter: Geolix.Adapter.MMDB2,
-      source: "/home/topaxi/diplomarbeit/tmp/geoip/GeoLite2-City.tar.gz"
+      source: System.get_env("GEOIP_DATABASE_FILE")
     }
   ]
 
